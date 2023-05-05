94421c771d8867e63e8cc400a698dba53d33601b
Pierre Romera <hello@pirhoo.com>
2023-05-05 09:23:26 +0000

    feat: hello world :alien:

--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,14 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# meta_topic_id: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
-# required_version: 2.7.0
+# name: icij-discourse-projects-next
+# about: A plugin for using groups to separate and organize categories and topics.
+# version: 0.0.0
+# authors: ICIJ <engineering@icij.org>
+# required_version: 3.4.0
 
-enabled_site_setting :plugin_name_enabled
-
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
-end
-
-require_relative "lib/my_plugin_module/engine"
 
 after_initialize do
-  # Code which should run after Rails has finished booting
+  add_class_method(:Group, :icij_projects) do
+    self.where(icij_group: true)
+  end
 end
