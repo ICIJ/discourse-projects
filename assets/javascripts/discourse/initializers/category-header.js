705d74aa34a5dc9e864baa475f9e96421e8e80dd
Caroline Desprat <cdesprat@icij.org>
2023-11-16 17:51:36 +0100

    feat: add I18n + restrict project categories to read_restricted ones + code comments

--- assets/javascripts/discourse/initializers/category-header.js
+++ assets/javascripts/discourse/initializers/category-header.js
@@ -1,43 +1,50 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import Category from "discourse/models/category";
 
-function isDefined(obj){
-  return obj !== null && typeof obj !== "undefined" 
-}
-function isTopicOrCategory(path){
-  return path.startsWith("discovery.category") || path.startsWith("topic.")
+function isDefined(obj) {
+  return obj !== null && typeof obj !== "undefined";
 }
 
-function initialize(api,container) {
+function isTopicOrCategory(path) {
+  return path.startsWith("discovery.category") || path.startsWith("topic.");
+}
 
+function isRelatedToProjectCategory(category) {
+  return category.read_restricted; // currently all read_restricted categories are considered realted a top category project
+}
+
+function initialize(api, container) {
   api.registerConnectorClass("above-main-container", "project-banner", {
-    pluginId: 'projects',
-  
+    pluginId: "projects",
+
     setupComponent(args, component) {
       api.onPageChange(async (url, title) => {
-        const nav = container.lookup('router:main')
-        
-        if (!isTopicOrCategory(nav.currentPath)) {
-          component.set("topCategory", null);
-          return;
-        }
-        
-        let category = container.lookup('controller:navigation/category')?.category 
-        if(!isDefined(category)){
-          const topic = container.lookup('controller:topic')?.get("model"); 
-          if(isDefined(topic)) {
-            const categoryId = topic.get("category_id");
-            category = Category.findById(categoryId);
+        const currentPath = container.lookup("router:main")?.currentPath
+        //project banner should only appear on pages related to a project: categories or topics
+        if (isTopicOrCategory(currentPath)) {
+          // check if we are on a category page
+          let category = container.lookup("controller:navigation/category")?.category;
+          
+          // if the category is not defined 
+          // it means that we may be on a topic page
+          if (!isDefined(category)) {
+            const topic = container.lookup("controller:topic")?.get("model");
+            if (isDefined(topic)) {
+              const categoryId = topic.get("category_id");
+              category = Category.findById(categoryId);
+            }
           }
-        } 
-
-        if(isDefined(category)) {
-          const topCategory = category.ancestors[0];
-          component.set("topCategory", topCategory);
-          return;
+          
+          // check the retrieved category is related to a project
+          if (isDefined(category) && isRelatedToProjectCategory(category)) {
+            // a category always have an ancestor (itself at least)
+            // so the first ancestor is the top category
+            const topCategory = category.ancestors[0]; 
+            component.set("topCategory", topCategory);
+            return;
+          }
         }
-        
-        throw new Error("Missed the path",nav.currentPath)
+        component.set("topCategory", null);
       });
     },
   });
@@ -46,6 +53,6 @@ function initialize(api,container) {
 export default {
   name: "categories-header",
   initialize(container) {
-    withPluginApi("1.8.0",  (api) => initialize(api, container));
+    withPluginApi("1.8.0", (api) => initialize(api, container));
   },
 };
