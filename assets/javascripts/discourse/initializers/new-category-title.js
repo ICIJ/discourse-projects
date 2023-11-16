f8e2e55550700629d4d398ed4f999856e94a3464
Pierre Romera <hello@pirhoo.com>
2023-11-16 18:12:06 +0100

    refactor: split initializer

--- assets/javascripts/discourse/initializers/new-category-title.js
+++ assets/javascripts/discourse/initializers/new-category-title.js
@@ -1,17 +1,52 @@
+import Category from "discourse/models/category";
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { not } from "@ember/object/computed";
-import  EditCategoryGeneral from 'discourse/components/edit-category-general'
-import { getOwner } from "@ember/application";
+import { observes, on } from "discourse-common/utils/decorators";
+import { computed } from "@ember/object";
+import { ajax } from "discourse/lib/ajax";
 
 function initialize(api) {
-  EditCategoryGeneral.reopen({
-    get canSelectParentCategory() {
-      return !this.isNewSubcategory && not("category.isUncategorizedCategory")
+
+
+  api.modifyClass("controller:edit-category-tabs", {
+    pluginId: 'projects',
+
+    @observes("model.parent_category_id")
+    async onParentCategoryChange() {
+      if(this.model.parent_category_id) {
+        const groupPermissions = await getCategoryGroupPermissions(this.model.parent_category_id);
+        // This ensure we do not cumulate new permissions with the existing one
+        this.model.permissions.clear();
+        // Then we add each permission one by one to ensure
+        // the set is used correctly
+        groupPermissions.forEach((permission) => {
+          this.model.addPermission(permission);
+        });
+      }
     },
-    get isNewSubcategory() {
-      return getOwner(this).lookup('router:main')?.currentRoute?.name === 'newSubcategory'
-    }
-  })
+
+    get hasParentCategory() {
+      return !!this.parentCategory
+    },
+
+    
+    @computed('model.parent_category_id')
+    get title() {
+      return this.hasParentCategory ? this.titleWithCategory : I18n.t("js.category.create")
+    },
+
+    @computed('model.parent_category_id')
+    get titleWithCategory() {
+      const { name: category } = this.parentCategory
+      const project = this.project
+      return I18n.t('js.subcategory.create', { category })
+    },  
+
+    @computed('model.parent_category_id')
+    get parentCategory() {
+      return Category.findById(this.model.parent_category_id)
+    },
+
+  });
 }
 
 export default {
