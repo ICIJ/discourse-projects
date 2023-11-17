3546cfcf8bab0ccf4140d5afc00fed73289ff98c
Pierre Romera <hello@pirhoo.com>
2023-11-17 13:20:12 +0100

    refactor: use new is_project and project fields

--- assets/javascripts/discourse/initializers/set-current-project.js
+++ assets/javascripts/discourse/initializers/set-current-project.js
@@ -9,10 +9,6 @@ function isTopicOrCategory(path) {
   return path.startsWith("discovery.category") || path.startsWith("topic.");
 }
 
-function isRelatedToProjectCategory(category) {
-  return category.read_restricted; // currently all read_restricted categories are considered realted a top category project
-}
-
 function initialize(api, container) {
   api.registerConnectorClass("above-main-container", "project-banner", {
     pluginId: "projects",
@@ -21,26 +17,30 @@ function initialize(api, container) {
       api.onPageChange(async (url, title) => {
         let currentProject = null
         const currentPath = container.lookup("router:main")?.currentPath
-        //project banner should only appear on pages related to a project: categories or topics
+        // Project banner should only appear on pages related to a project: categories or topics
         if (isTopicOrCategory(currentPath)) {
-          // check if we are on a category page
+          // Check if we are on a category page
           let category = container.lookup("controller:navigation/category")?.category;
           
-          // if the category is not defined 
+          // If the category is not defined 
           // it means that we may be on a topic page
           if (!isDefined(category)) {
             const topic = container.lookup("controller:topic")?.get("model");
+
             if (isDefined(topic)) {
               const categoryId = topic.get("category_id");
               category = Category.findById(categoryId);
             }
           }
-          
-          // check the retrieved category is related to a project
-          if (isDefined(category) && isRelatedToProjectCategory(category)) {
-            // a category always have an ancestor (itself at least)
-            // so the first ancestor is the top category
-            currentProject = category.ancestors[0]; 
+
+          if (isDefined(category)) {
+            // Check if the category itself is a project
+            if (category.is_project) {
+              currentProject = category;
+            // Check the retrieved category is related to a project
+            } else if (isDefined(category.project)) {
+              currentProject = category.project;
+            }           
           }
         }
         component.set("currentProject", currentProject);
