ed5d29538e8e605509d7218f52c7c0612b03b598
Pierre Romera <hello@pirhoo.com>
2023-11-17 12:33:36 +0000

    feat: add alternate title for sub categories

--- assets/javascripts/discourse/initializers/new-category-title.js
+++ assets/javascripts/discourse/initializers/new-category-title.js
@@ -12,13 +12,19 @@ function initialize(api) {
     
     @computed('model.parent_category_id')
     get title() {
-      return this.hasParentCategory ? this.titleWithCategory : I18n.t("js.category.create")
+      if (!this.project) {
+        return I18n.t("js.category.create")
+      }
+      if (this.project.id === this.parentCategory.id) {
+        return this.titleWithProject
+      }
+      return this.titleWithCategory
     },
 
     @computed('model.parent_category_id')
     get titleWithCategory() {
-      const { name: category } = this.parentCategory
-      return I18n.t('js.subcategory.create', { category })
+      const { name: category, project: { name: project } } = this.parentCategory
+      return I18n.t('js.subcategory.create', { category, project })
     },  
 
     @computed('model.parent_category_id')
@@ -30,6 +36,14 @@ function initialize(api) {
     @computed('model.parent_category_id')
     get parentCategory() {
       return Category.findById(this.model.parent_category_id)
+    },
+
+    @computed('model.parent_category_id')
+    get project() {
+      if (this.parentCategory?.is_project) {
+        return this.parentCategory
+      }
+      return this.parentCategory?.project
     },
 
   });
