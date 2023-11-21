50fa7113ae9bd6b4c26c0bcff5d7be8665490142
Pierre Romera <hello@pirhoo.com>
2023-11-21 11:27:04 +0000

    refactor: lint

--- lib/projects/engine.rb
+++ lib/projects/engine.rb
@@ -2,6 +2,6 @@ module ::Projects
   class Engine < ::Rails::Engine
     engine_name PLUGIN_NAME
     isolate_namespace Projects
-    config.autoload_paths << File.join(config.root, "lib")
+    config.autoload_paths << File.join(config.root, 'lib')
   end
 end
