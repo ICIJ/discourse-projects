f284841121cb2c3a3503da325a51b789e5a31346
Pierre Romera <hello@pirhoo.com>
2023-11-22 15:53:15 +0000

    refactor: lint

--- test/javascripts/components/new-subcategory-button-test.js
+++ test/javascripts/components/new-subcategory-button-test.js
@@ -8,8 +8,12 @@ module("Projects | Component | NewSubcategoryButton", function (hooks) {
   setupRenderingTest(hooks);
 
   test("when button is rendered", async function (assert) {
-    sinon.stub(this.owner.lookup("controller:discovery"), "category").value(null);
+    sinon
+      .stub(this.owner.lookup("controller:discovery"), "category")
+      .value(null);
     await render(hbs`<NewSubcategoryButton />`);
-    assert.dom(".new-subcategory-button").exists("it shows the new subcategory button");
+    assert
+      .dom(".new-subcategory-button")
+      .exists("it shows the new subcategory button");
   });
 });
--- test/javascripts/components/project-banner-test.js
+++ test/javascripts/components/project-banner-test.js
@@ -7,16 +7,20 @@ module("Projects | Component | ProjectBanner", function (hooks) {
   setupRenderingTest(hooks);
 
   this.category = {
-    id: '1',
-    slug: 'foo',
-    name: 'Foo'
+    id: "1",
+    slug: "foo",
+    name: "Foo",
   };
 
   test("when banner is rendered", async function (assert) {
     await render(hbs`<ProjectBanner @category={{this.category}}/>`);
     assert.dom(".project-banner__wrapper").exists("it shows a project banner");
-    assert.dom(".project-banner__wrapper__link").exists("it shows a link to the corresponding category");
-    assert.dom(".project-banner__wrapper__link").hasText('Foo');
-    assert.dom(".project-banner__wrapper__link").hasAttribute('href', '/c/foo/1');
+    assert
+      .dom(".project-banner__wrapper__link")
+      .exists("it shows a link to the corresponding category");
+    assert.dom(".project-banner__wrapper__link").hasText("Foo");
+    assert
+      .dom(".project-banner__wrapper__link")
+      .hasAttribute("href", "/c/foo/1");
   });
 });
