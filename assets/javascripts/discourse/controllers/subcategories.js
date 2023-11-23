aae62c66ae8f14e10e9636a37ab1daa7e256ecd3
Pierre Romera <hello@pirhoo.com>
2023-11-23 16:47:47 +0100

    feat: add subcategories page

---
+++ assets/javascripts/discourse/controllers/subcategories.js
@@ -0,0 +1,52 @@
+import { action, computed } from "@ember/object";
+import { inject as service } from "@ember/service";
+import Category from "discourse/models/category";
+import DiscourseURL from "discourse/lib/url";
+import NavItem from "discourse/models/nav-item";
+import Controller from "@ember/controller";
+
+export default class SubcategoriesController extends Controller {
+  categories = Category.list();
+
+  @service router;
+  @service modal;
+
+  @computed("categories", "category")
+  get subcategories() {
+    return this.categories.filter(({ parent_category_id: parent }) => {
+      return parent === this.category?.id
+    })
+  }
+
+  @computed("subcategories")
+  get hasSubcategories() {
+    return this.subcategories.length > 0;
+  }
+
+  @computed("model")
+  get category() {
+    return this.model ?? null
+  }
+
+  @computed("category")
+  get navItems() {
+    return NavItem.buildList(this.category, {
+      filterType: 'latest',
+      noSubcategories: false,
+      currentRouteQueryParams: this.router.currentRoute.queryParams,
+      tagId: null,
+      siteSettings: this.siteSettings,
+      skipCategoriesNavItem: true,
+    });
+  }
+
+  @action
+  editCategory() {
+    DiscourseURL.routeTo(`/c/${Category.slugFor(this.category)}/edit`);
+  }
+
+  @action
+  createCategory() {
+    this.router.transitionTo("newCategory");
+  }
+}
