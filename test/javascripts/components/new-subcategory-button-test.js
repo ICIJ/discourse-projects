61ad5811da625589c75847671f278e38d2597416
Pierre Romera <hello@pirhoo.com>
2023-11-24 15:15:41 +0100

    test: remove legacy stub

--- test/javascripts/components/new-subcategory-button-test.js
+++ test/javascripts/components/new-subcategory-button-test.js
@@ -8,9 +8,6 @@ module("Projects | Component | NewSubcategoryButton", function (hooks) {
   setupRenderingTest(hooks);
 
   test("when button is rendered", async function (assert) {
-    sinon
-      .stub(this.owner.lookup("controller:discovery"), "category")
-      .value(null);
     await render(hbs`<NewSubcategoryButton />`);
     assert
       .dom(".new-subcategory-button")
