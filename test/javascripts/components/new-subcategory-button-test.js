ca337a3f4f691d7b7aa34a51d93bd9041adbb302
Pierre Romera <hello@pirhoo.com>
2023-11-24 15:28:10 +0100

    refactor: lint

--- test/javascripts/components/new-subcategory-button-test.js
+++ test/javascripts/components/new-subcategory-button-test.js
@@ -1,7 +1,6 @@
 import { render } from "@ember/test-helpers";
 import hbs from "htmlbars-inline-precompile";
 import { module, test } from "qunit";
-import sinon from "sinon";
 import { setupRenderingTest } from "discourse/tests/helpers/component-test";
 
 module("Projects | Component | NewSubcategoryButton", function (hooks) {
