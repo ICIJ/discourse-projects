8a82436c1b997c679cfd7984426fd3e3fb97b09a
Pierre Romera <hello@pirhoo.com>
2023-11-27 18:47:53 +0100

    refactor: use route params to retreive current project

--- assets/javascripts/discourse/helpers/is-undefined.js
+++ assets/javascripts/discourse/helpers/is-undefined.js
@@ -1,3 +1,8 @@
+/**
+ * Return true if the given value is undefined.
+ * @param {*} value - The value to check.
+ * @returns {Boolean}
+ */
 export default function isUndefined(value) {
   return value === undefined;
 }
