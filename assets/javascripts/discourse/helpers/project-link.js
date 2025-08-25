8620ecb8c6fae9f4e11c3b28336b2359f69f5e24
Pierre Romera <hello@pirhoo.com>
2025-08-25 18:18:58 +0200

    feat: customize project links

--- assets/javascripts/discourse/helpers/project-link.js
+++ assets/javascripts/discourse/helpers/project-link.js
@@ -1,12 +1,17 @@
 import { categoryLinkHTML } from "discourse/helpers/category-link";
+import { helperContext } from "discourse/lib/helpers";
+
+const PROJECT_LINK_CLASS = "project-link";
+const PROJECT_LINK_STYLED_CLASS = `${PROJECT_LINK_CLASS} ${PROJECT_LINK_CLASS}--styled`;
 
 export function projectLinkHTML(project, options = {}) {
-  const { extraClasses = "" } = options;
-  const projectExtraClasses = "project-link";
-  return categoryLinkHTML(project, {
-    ...options,
-    extraClasses: `${extraClasses} ${projectExtraClasses}`,
-  });
+  const { siteSettings } = helperContext();
+  const styled = siteSettings.projects_styled_links ?? false;
+  const projectExtraClasses = styled
+    ? PROJECT_LINK_STYLED_CLASS
+    : PROJECT_LINK_CLASS;
+  const extraClasses = `${options.extraClasses} ${projectExtraClasses}`;
+  return categoryLinkHTML(project, { ...options, extraClasses });
 }
 
 export default projectLinkHTML;
