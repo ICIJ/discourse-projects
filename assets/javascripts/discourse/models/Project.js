bba74a7aaba44501e1efbc68167dafaeffd4e6fd
Pierre Romera <hello@pirhoo.com>
2025-08-27 12:30:35 +0200

    feat: add asyncSearch and ensure projects are in loaded categories

--- assets/javascripts/discourse/models/Project.js
+++ assets/javascripts/discourse/models/Project.js
@@ -2,7 +2,8 @@ import { ajax } from "discourse/lib/ajax";
 import PreloadStore from "discourse/lib/preload-store";
 import Category from "discourse/models/category";
 import CategoryList from "discourse/models/category-list";
-import RestModel from "discourse/models/rest";
+import RestModel from "discourse/models/rest"
+import Site from "discourse/models/site";
 
 export default class Project extends RestModel {
   static async findAll() {
@@ -11,7 +12,12 @@ export default class Project extends RestModel {
       const { projects = [] } = pre
         ? { projects: pre }
         : await ajax("/projects.json");
-      return projects.map((p) => Category.create(p));
+      // We must convert the project objects to categories in order to update the site
+      // store for categories. This will allow to the category helpers and components to
+      // display project correctly.
+      const categories = projects.map((p) => Category.create(p));
+      categories.forEach((cat) => Site.current().updateCategory(cat));
+      return categories;
     } catch {
       return [];
     }
@@ -22,5 +28,16 @@ export default class Project extends RestModel {
     const list = CategoryList.create();
     projects.forEach((p) => list.pushObject(p));
     return list;
+  }
+
+  static async asyncSearch(filter = '') {
+    const projects = await Project.findAll();
+    const needle = filter.toLowerCase().trim();
+    const match = ({ name, slug }) => {
+      return !filter 
+      || name.toLowerCase().includes(needle) 
+      || slug.toLowerCase().includes(needle);
+    };
+    return projects.filter(match);
   }
 }
