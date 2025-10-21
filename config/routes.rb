320b20affdd9ab607b31a90317fc2e0890bd3f40
Pierre Romera <hello@pirhoo.com>
2025-10-21 17:02:58 +0200

    feat: register routes for project's member and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 DiscourseProjects::Engine.routes.draw do
   get "/projects" => "projects#index"
+  get "/projects/:slug" => "projects#show"
+  get "/projects/:slug/members" => "projects#members"
 end
 
 Discourse::Application.routes.draw { mount ::DiscourseProjects::Engine, at: "/" }
