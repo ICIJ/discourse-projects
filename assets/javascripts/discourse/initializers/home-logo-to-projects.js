445673104fdcbd086b20e76e4c62dea938ce4ad8
Pierre Romera <hello@pirhoo.com>
2025-10-31 15:15:25 +0000

    refactor: lint

--- assets/javascripts/discourse/initializers/home-logo-to-projects.js
+++ assets/javascripts/discourse/initializers/home-logo-to-projects.js
@@ -1,19 +1,21 @@
-import { withPluginApi } from "discourse/lib/plugin-api"
+import { withPluginApi } from "discourse/lib/plugin-api";
 
-/** 
- * The role of this initializer is to change the homepage 
+/**
+ * The role of this initializer is to change the homepage
  * logo link to point to /projects when the site setting is enabled.
  */
 function initialize(api) {
-  const SITE_SETTING_NAME = "projects_home_logo_to_projects"
-  const { [SITE_SETTING_NAME]: toggle } = api.container.lookup("service:site-settings")
-  api.registerHomeLogoHrefCallback(() => toggle ? '/projects' : '/')
+  const SITE_SETTING_NAME = "projects_home_logo_to_projects";
+  const { [SITE_SETTING_NAME]: toggle } = api.container.lookup(
+    "service:site-settings"
+  );
+  api.registerHomeLogoHrefCallback(() => (toggle ? "/projects" : "/"));
 }
 
 export default {
   name: "home-logo-to-projects",
 
   initialize() {
-    withPluginApi(initialize)
+    withPluginApi(initialize);
   },
-}
+};
