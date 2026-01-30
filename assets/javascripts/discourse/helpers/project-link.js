fbac7d00ba1892e90a056750ace5c7539cf9c06f
Pierre Romera <hello@pirhoo.com>
2026-01-30 14:59:18 +0000

    feat: add projectPath and projectUrl helper functions #14

--- assets/javascripts/discourse/helpers/project-link.js
+++ assets/javascripts/discourse/helpers/project-link.js
@@ -1,8 +1,20 @@
+import { htmlSafe } from "@ember/template";
 import { categoryLinkHTML } from "discourse/helpers/category-link";
 import { helperContext } from "discourse/lib/helpers";
 
 const PROJECT_LINK_CLASS = "project-link";
 const PROJECT_LINK_STYLED_CLASS = `${PROJECT_LINK_CLASS} ${PROJECT_LINK_CLASS}--styled`;
+
+export function projectPath(project, { subcategories = false } = {}) {
+  const basePath = `/c/${project.slug}/${project.id}`;
+  return subcategories ? `${basePath}/subcategories` : basePath;
+}
+
+export function projectUrl(project) {
+  const { siteSettings } = helperContext();
+  const subcategories = siteSettings.projects_link_to_subcategories;
+  return projectPath(project, { subcategories });
+}
 
 export function projectLinkHTML(project, options = {}) {
   const { siteSettings } = helperContext();
@@ -11,7 +23,25 @@ export function projectLinkHTML(project, options = {}) {
     ? PROJECT_LINK_STYLED_CLASS
     : PROJECT_LINK_CLASS;
   const extraClasses = `${options.extraClasses} ${projectExtraClasses}`;
-  return categoryLinkHTML(project, { ...options, extraClasses });
+  const html = categoryLinkHTML(project, { ...options, extraClasses });
+
+  // If enabled, redirect project links to subcategories page
+  if (
+    siteSettings.projects_link_to_subcategories &&
+    project?.slug &&
+    project?.id
+  ) {
+    const basePath = projectPath(project);
+    const htmlUnsafe = html.toString();
+    // Replace only the href attribute to avoid messing with other parts of the HTML
+    return htmlSafe(
+      htmlUnsafe
+        .split(`href="${basePath}"`)
+        .join(`href="${projectUrl(project)}"`)
+    );
+  }
+
+  return html;
 }
 
 export default projectLinkHTML;
