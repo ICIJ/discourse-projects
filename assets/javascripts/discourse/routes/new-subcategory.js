d54fb1b88834c425ea8a5c30b0dde740e49f66fe
Pierre Romera <hello@pirhoo.com>
2026-02-11 18:06:27 +0100

    fix: load parent category instead of just reading the store

--- assets/javascripts/discourse/routes/new-subcategory.js
+++ assets/javascripts/discourse/routes/new-subcategory.js
@@ -1,23 +1,24 @@
+import { ajax } from "discourse/lib/ajax";
+import Site from "discourse/models/site";
 import NewCategoryRoute from "discourse/routes/new-category";
-import matches from "../helpers/matches";
 
 export default class NewSubcategoryRoute extends NewCategoryRoute {
   async model(params) {
     const model = await super.model(...arguments);
-    model.set("parent_category_id", this.getParentCategoryId(params));
+    try {
+      const { category_id: categoryId } = params;
+      const { category } = await ajax(`/c/${categoryId}/show.json`);
+      // This will update the form's model value for the parent category
+      model.set("parent_category_id", category.id);
+      // Make sure the parent category is up to date in the store
+      Site.current().updateCategory(category);
+    } catch {
+      model.set("parent_category_id", null);
+    }
     return model;
   }
 
   defaultGroupPermissions() {
     return [];
-  }
-
-  categoryExists(id) {
-    return this.site.categories.some(matches({ id }));
-  }
-
-  getParentCategoryId({ category_id: id }) {
-    const parentCategoryId = parseInt(id, 10);
-    return this.categoryExists(parentCategoryId) ? parentCategoryId : null;
   }
 }
