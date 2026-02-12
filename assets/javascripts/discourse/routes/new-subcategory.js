2bcee7aae94adec9390e7ae0b8cc2345efee1a75
Pierre Romera <hello@pirhoo.com>
2026-02-12 22:37:29 +0100

    refactor: lint

--- assets/javascripts/discourse/routes/new-subcategory.js
+++ assets/javascripts/discourse/routes/new-subcategory.js
@@ -2,7 +2,6 @@ import { ajax } from "discourse/lib/ajax";
 import { SEARCH_PRIORITIES } from "discourse/lib/constants";
 import Site from "discourse/models/site";
 import NewCategoryRoute from "discourse/routes/new-category";
-import { addObserver } from "@ember/object/observers";
 
 export default class NewSubcategoryRoute extends NewCategoryRoute {
   async model(params) {
