86ab158d1ef45d27f51b1bbd9619a149006d8287
Pierre Romera <hello@pirhoo.com>
2026-02-20 08:55:41 +0000

    feat: unselecting a project in Discovery should lead to the projects page #19

--- assets/javascripts/discourse/components/project-dropdown.js
+++ assets/javascripts/discourse/components/project-dropdown.js
@@ -1,6 +1,7 @@
-import { computed } from "@ember/object";
+import { action, computed } from "@ember/object";
 import { service } from "@ember/service";
 import { classNames } from "@ember-decorators/component";
+import DiscourseURL from "discourse/lib/url";
 import { i18n } from "discourse-i18n";
 import CategoryDrop from "select-kit/components/category-drop";
 import {
@@ -19,6 +20,7 @@ import ProjectDropdownSelectedName from "./project-dropdown/project-dropdown-sel
 })
 export default class ProjectDropdown extends CategoryDrop {
   @service project;
+  @service siteSettings;
 
   noCategoriesLabel = i18n("js.project_dropdown.select");
 
@@ -32,5 +34,19 @@ export default class ProjectDropdown extends CategoryDrop {
       return this.content;
     }
     return Project.asyncSearch(filter);
+  }
+
+  /**
+   * When the user clears the dropdown and the setting `projects_home_logo_to_projects` is enabled,
+   * we want to route them to the projects home page. Otherwise, we fallback to the default
+   * behavior of clearing the category selection.
+   */
+  @action
+  onChange(categoryId) {
+    if (!categoryId && this.siteSettings.projects_home_logo_to_projects) {
+      DiscourseURL.routeToUrl("/projects");
+      return;
+    }
+    super.onChange(categoryId);
   }
 }
