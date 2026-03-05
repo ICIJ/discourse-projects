4479444b91a9718abca9e7db4411dce58b56821a
Pierre Romera <hello@pirhoo.com>
2026-03-05 17:38:38 +0000

    feat: validate topics cannot be created in project categories #22

--- lib/discourse_projects/topic_extension.rb
+++ lib/discourse_projects/topic_extension.rb
@@ -4,8 +4,22 @@ module DiscourseProjects
   module TopicExtension
     extend ActiveSupport::Concern
 
+    prepended do
+      validate :topic_not_in_project_category, on: :create
+    end
+
     def project
       category&.project? ? category : category&.project
+    end
+
+    private
+
+    def topic_not_in_project_category
+      return unless category&.project?
+      return unless SiteSetting.projects_enabled
+      return unless SiteSetting.projects_composer_project_chooser
+
+      errors.add(:base, I18n.t("discourse_projects.errors.topic_in_project_category"))
     end
   end
 end
